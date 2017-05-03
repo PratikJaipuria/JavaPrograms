19286e7e8c4ec0d98136fac90bc7fd9ebf599084
jaipuriapratik <jaipuria.p@husky.neu.edu>
2017-05-03 18:46:23 -0400

    Basic data types

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Pratik on 4/29/2017.
+ */
+public class Hello {
+    public static void main(String[] args){
+
+
+        System.out.println("Hello World!!");
+
+        int myFirstNumber = (5 + 10)/2 + 10*3;
+        System.out.println(myFirstNumber);
+
+        float mySecondNumber = (5 + 10)/2 + 10*3;
+        System.out.println(mySecondNumber);
+
+    }
+}
